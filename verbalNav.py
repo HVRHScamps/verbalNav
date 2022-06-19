568b88f5f1b883a5c4b9863094137dec7b0d2b9c
cannotilever <cannotilever@protonmail.com>
2022-06-19 14:05:43 -0400

    fix syntax error

--- verbalNav.py
+++ verbalNav.py
@@ -2,4 +2,4 @@ import libhousy
 #You can define helper functions here, make sure to but them *above* the main function
 def main(robot: libhousy.robot):
     #Here is where your recurring code will go
-    print("Hello World!")z
+    print("Hello World!")
