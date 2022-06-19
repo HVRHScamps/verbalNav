56d08ddd0623e857d6fab48c23de8b287f67191a
cannotilever <cannotilever@protonmail.com>
2022-06-19 13:18:38 -0400

    initial setup

---
+++ verbalNav.py
@@ -0,0 +1,5 @@
+import libhousy
+#You can define helper functions here, make sure to but them *above* the main function
+def main(robot: libhousy.robot):
+    #Here is where your recurring code will go
+    print("Hello World!")z
