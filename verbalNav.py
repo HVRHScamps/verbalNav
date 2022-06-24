f0aaa4fd9a69e2573cd6a4d1f26c320b91c662fc
Sydney <sydneyhoweacademy@gmail.com>
2022-06-24 09:36:34 -0400

    forgot to check for problems within the code

--- verbalNav.py
+++ verbalNav.py
@@ -11,7 +11,7 @@ def turn(angle, direction):
     
 def main(robot: libhousy.robot):
     global forward
-    #Here is where your recurring code will go
+#Here is where your recurring code will go
     command = input("Type a command here").split(" ")
     if command[0] == "drive":
         dist = int(command[2])
@@ -19,3 +19,4 @@ def main(robot: libhousy.robot):
     elif command[0] == "turn":
         angle = int(command[2])
         turn(angle, command[1])
+        
