283c3ae9b0fa969ec7e658e05df3929b61712318
Sydney <sydneyhoweacademy@gmail.com>
2022-06-24 09:17:30 -0400

    changing the angle and drive

--- verbalNav.py
+++ verbalNav.py
@@ -1,5 +1,17 @@
 import libhousy
+
+forward = True
 #You can define helper functions here, make sure to but them *above* the main function
 def main(robot: libhousy.robot):
+    global forward
     #Here is where your recurring code will go
-    print("Hello World!")
+
+    command = input("Type a command here").split(" ")
+    if command[0] == "drive":
+        
+        dist = int(command[2])
+        drive(dist)
+    elif command[0] == "turn":
+        angle = int(command[2])
+        turn(angle, command[1])
+        
